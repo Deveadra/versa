92488d3d6d66e7635af90eaf501bcc23470aa438
Sundance <tyreabc@gmail.com>
2026-03-03 21:59:31 -0500

    Fixed personality commands

--- src/personalities/__init__.py
+++ src/personalities/__init__.py
@@ -1,9 +1,9 @@
-from personalities.aerith.lines import JARVIS_PERSONALITY
+from personalities.jarvis.lines import JARVIS_PERSONALITY
 from personalities.aerith.lines import AERITH_PERSONALITY
 
-PERSONALITIES = {"aerith": JARVIS_PERSONALITY, "aerith": AERITH_PERSONALITY}
+PERSONALITIES = {"jarvis": JARVIS_PERSONALITY, "aerith": AERITH_PERSONALITY}
 
 # Default active personality is now Aerith
 CURRENT_PERSONALITY = PERSONALITIES["aerith"]
 
-PERSONALITY_COMMANDS = {"switch to aerith": "aerith", "switch to aerith": "aerith"}
+PERSONALITY_COMMANDS = {"switch to jarvis": "jarvis", "switch to aerith": "aerith"}
